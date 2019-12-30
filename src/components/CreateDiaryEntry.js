a5b4c65f982b5515b62eb13752b09d5497be0907
David_Cech <david.cech@student.gyarab.cz>
2019-12-30 11:55:39 +0100

    Worked on CreateDiaryEntry, updated generation of the html table, added buttons which remove items from the table, started working on submitting diaryEntry.

--- src/components/CreateDiaryEntry.js
+++ src/components/CreateDiaryEntry.js
@@ -13,6 +13,7 @@ class CreateDiaryEntry extends Component {
         this.handleSelect = this.handleSelect.bind(this);
         this.changeGrams = this.changeGrams.bind(this);
         this.addFood = this.addFood.bind(this);
+        this.removeFood = this.removeFood.bind(this);
     }
 
     state = {
@@ -94,21 +95,23 @@ class CreateDiaryEntry extends Component {
         },
         mealName: "no_select",
         grams: "",
-        tableHtml: <div />,
+        tableHtml: <div/>,
     };
 
+    //Generates empty table of meals upon mounting
+    componentDidMount() {
+        this.generateTable(Object.entries(this.state.addedFoods));
+    }
 
+    //Adds food to a given meal array in addedFoods attribute of state, requires mealName and grams to be assigned
+    //values by the user through and html form
     addFood = (event) => {
         event.preventDefault();
         if (this.props.searchedFood !== null && this.state.grams !== "" && this.state.mealName !== "no_select") {
-            console.log(this.state.grams, this.props.searchedFood.name, this.state.mealName);
             let keys = Object.keys(this.state.addedFoods);
             let entries = Object.entries(this.state.addedFoods);
-            console.log(keys);
-            keys.map(key => {
+            keys.forEach(key => {
                 if (key.toString() === this.state.mealName) {
-                    console.log(keys.indexOf(key));
-                    console.log(entries[keys.indexOf(key)][2]);
                     entries[keys.indexOf(key)][1].push({food: this.props.searchedFood, grams: this.state.grams});
                 }
             });
@@ -119,38 +122,47 @@ class CreateDiaryEntry extends Component {
         }
     };
 
+    //Changes the number of grams of a food in state when user inputs grams in the html form
     changeGrams = (event) => {
-        console.log(event.target.value);
         this.setState({grams: event.target.value});
     };
 
-    //Meni atribut mealName ve state podle vyberu uzivatele
+    //Changes attribute mealName in state when user selects one of the options in the html form
     handleSelect = (event) => {
-        console.log(event.target.value);
         this.setState({mealName: event.target.value})
     };
 
+    //Changes attribute date in state when user selects one of the dates in the html form
     changeDate = (date) => {
         this.setState({date: date, diaryEntry: {...this.state.diaryEntry, date: date}});
-        console.log(this.state.diaryEntry);
     };
 
+    //NEEEEEEEEEEEEEDSSSSS UPDAAAAAAATE
+    //Checks whether user input all necessary data, calls function xxx from diaryEntryActionCreator
     handleSubmit = () => {
         let diaryEntry = this.state.diaryEntry;
         if (diaryEntry.date instanceof Date) {
             let singleDay = diaryEntry.date;
-            console.log(diaryEntry);
             createDiaryEntry(this.state.diaryEntry);
         } else {
             console.log("Musite zvolit datum nebo data v kalendari")
         }
     };
 
+    //Removes food from attribute addedFoods in state at an index given by the foodIndex argument
+    removeFood = (foodIndex) =>{
+        let entries = Object.entries(this.state.addedFoods);
+        entries[foodIndex[0]][1].splice(foodIndex[1],1);
+        this.generateTable(entries);
+        this.setState({addedFoods: Object.fromEntries(entries)})
+    };
+
+    //Probably does nothing serves debugging purposes
     componentDidUpdate(prevProps, prevState) {
         console.log(this.state.mealName !== "no_select");
         console.log(this.props.searchedFood !== prevProps.searchedFood);
         if (this.props.searchedFood !== prevProps.searchedFood && this.state.mealName !== "no_select") {
-            console.log(this.props.searchedFood, this.state.mealName)
+            console.log(this.props.searchedFood, this.state.mealName);
         }
     }
 
@@ -188,42 +200,76 @@ class CreateDiaryEntry extends Component {
         )
     }
 
+    // Generates an html table the columns and rows of which are set by the number of meals and number of elements inside
+    // the arrays of those meals respectively, meals and their arrays are stored in addedFood attribute of state
     generateTable = (entries) => {
         console.log(entries, "Generuji");
+        //Determines number of rows table will have
         let longest = -1;
-        for(let i = 0; i < entries.length; i++){
-            console.log(entries[i][1].length);
-            if(entries[i][1].length > longest){
+        for (let i = 0; i < entries.length; i++) {
+            if (entries[i][1].length > longest) {
                 longest = entries[i][1].length
             }
         }
-        console.log(longest);
 
         let cells;
         let rows = [];
-        //tr tag needed
-        let mealNames = ["Breakfast", "Morning snack", "Lunch", "Afternoon snack", "Dinner", "Other"];
+        let mealNames = ["Snidane", "Dopoledni svacina", "Obed", "Odpoledni svacina", "Vecere", "Ostatni"];
 
-        for(let i = 0; i <= longest; i++){
+        //i represents number of columns rows and j number of columns, if the meals arrays are empty it generates a
+        //placeholder instead
+        for (let i = 0; i <= longest; i++) {
             cells = [];
-            for (let j = 0; j<entries.length; j++){
-                if(i===0){
-                    cells.push(<th>{mealNames[j]}</th>);
+            for (let j = 0; j < entries.length; j++) {
+                if (i === 0) {
+                    cells.push(<th key={(i+1)*(j+1)}>{mealNames[j]}</th>);
                 } else {
-                    if(entries[j][1].length > i-1) {
+                    if (entries[j][1].length > i - 1) {
                         console.log(entries[j][1]);
-                        cells.push(<td>{entries[j][1][i-1].grams}</td>)
-                    } else{
-                        cells.push(<td />)
+                        cells.push(
+                            <td key={(i+1)*(j+1)}>
+                                {entries[j][1][i - 1].grams}
+                                <RemoveFoodButton foodIndex={[j,i-1]} onClick={this.removeFood}/>
+                            </td>
+                        )
+                    } else if(i===1){
+                        cells.push(<td key={(i+1)*(j+1)}>{"Pridejte chod"}</td>)
+                    } else {
+                        cells.push(<td key={(i+1)*(j+1)}/>)
                     }
                 }
             }
-            rows.push(<tr>{cells}</tr>)
-        }
-        let table = <table><tbody>{rows}</tbody></table>;
+            rows.push(<tr key={i+longest*2}>{cells}</tr>);
 
-        console.log(cells);
+            //if the arrays are empty inserts a placeholder
+            if (longest === 0) {
+                cells = [];
+                for (let j = 0; j < 6; j++) {
+                    cells.push(<td key={(i+1)*(j+1)}>{"Pridejte chod"}</td>)
+                }
+                rows.push(<tr key={i+longest*2+1}>{cells}</tr>);
+            }
+        }
+
+        let table = <table>
+            <tbody>{rows}</tbody>
+        </table>;
+
         this.setState({tableHtml: table})
+    }
+}
+
+//Button for removing foods from table, receives arguments foodIndex and onClick, foodIndex is the entry index of the
+//food which is to be removed from addedFoods, onClick is the function that is to be called onClick
+class RemoveFoodButton extends Component {
+    handleClick = () =>{
+        this.props.onClick(this.props.foodIndex);
+    };
+
+    render(){
+        return(
+            <button  onClick={this.handleClick}>X</button>
+        )
     }
 }
 
