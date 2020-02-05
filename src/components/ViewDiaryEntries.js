64905be450f5982a64422b41c6e4f67c28b174b8
David_Cech <david.cech@student.gyarab.cz>
2020-02-05 21:53:29 +0100

    ViewDiaryEntries should be done, at least for now. Fixed showing negative values, implemented showing fetchedData, for single dates generates table of what the user ate that day and his activities. Added labels to X and Y axes.

--- src/components/ViewDiaryEntries.js
+++ src/components/ViewDiaryEntries.js
@@ -15,70 +15,36 @@ class ViewDiaryEntries extends Component {
         this.handleCheckbox = this.handleCheckbox.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSelect = this.handleSelect.bind(this);
+        this.generateTable = this.generateTable.bind(this);
     }
 
     state = {
         date: null,
         multipleSelection: false,
         show: "kcal",
-        data: [
-            {
-                kcal: 88,
-                protein: 11,
-                carbs: 8,
-                fat: 4,
-                fibre: 34,
-                goalKcal: 2462,
-                goalProtein: 289,
-                goalCarbs: 292,
-                goalFat: 296,
-                goalFibre: 266,
-                date: '14. 1. 2020'
-            },
-            {
-                kcal: 184,
-                protein: 4,
-                carbs: 10,
-                fat: 3,
-                fibre: 11,
-                goalKcal: 2366,
-                goalProtein: 296,
-                goalCarbs: 290,
-                goalFat: 297,
-                goalFibre: 289,
-                date: '15. 1. 2020'
-            },
-            {
-                kcal: 0,
-                protein: 0,
-                carbs: 0,
-                fat: 0,
-                fibre: 0,
-                goalKcal: 2550,
-                goalProtein: 300,
-                goalCarbs: 300,
-                goalFat: 300,
-                goalFibre: 300,
-                date: '16. 1. 2020'
-            },
-            {
-                kcal: 0,
-                protein: 0,
-                carbs: 0,
-                fat: 0,
-                fibre: 0,
-                goalKcal: 2550,
-                goalProtein: 300,
-                goalCarbs: 300,
-                goalFat: 300,
-                goalFibre: 300,
-                date: '17. 1. 2020'
-            }
-
-
-        ]
-
+        data: [],
+        additionalData: null,
+        tableHtml: <div/>,
     };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.searchedDiaryEntries !== prevProps.searchedDiaryEntries && this.props.searchedDiaryEntries !== null && Array.isArray(this.props.searchedDiaryEntries)) {
+            this.setState({data: this.props.searchedDiaryEntries, tableHtml: <div/>});
+        } else if (this.props.searchedDiaryEntries !== prevProps.searchedDiaryEntries && this.props.searchedDiaryEntries !== null) {
+            if (this.props.searchedDiaryEntries.hasOwnProperty('enrichedData')) {
+                this.setState({
+                    data: [this.props.searchedDiaryEntries.data],
+                    additionalData: this.props.searchedDiaryEntries.enrichedData,
+                });
+                this.generateTable(this.props.searchedDiaryEntries.enrichedData);
+            } else {
+                this.setState({
+                    data: [this.props.searchedDiaryEntries],
+                    tableHtml: <div>Žádná data k zobrazení</div>,
+                });
+            }
+        }
+    }
 
     //Generates unique keys for html elements
     getKey = () => {
@@ -109,12 +75,12 @@ class ViewDiaryEntries extends Component {
         this.setState({show: event.target.value})
     };
 
-    renderLegendText(value, entry){
-        let noRender = ["goalKcal","goalProtein","goalCarbs","goalFibre","goalFat"];
-        const { color } = entry;
+    renderLegendText(value, entry) {
+        let noRender = ["goalKcal", "goalProtein", "goalCarbs", "goalFibre", "goalFat"];
+        const {color} = entry;
 
-        if(value && !noRender.includes(value)){
-            return <span style={{ color }}>{value}</span>;
+        if (value && !noRender.includes(value)) {
+            return <span style={{color}}>{value}</span>;
         }
 
         return null;
@@ -131,18 +97,28 @@ class ViewDiaryEntries extends Component {
         //Shows either chart for calories or nutrition values which depends on show attribute of state which user changes
         //with html select tag
         let barsHtml = [];
-        if(this.state.show === 'kcal'){
-            barsHtml.push(<Bar dataKey="kcal" fill="#18139c" stroke="#18139c" stackId="a" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="goalKcal" fill="none" stroke="#18139c" stackId="a" key={this.getKey()} isAnimationActive={false}/>);
+        if (this.state.show === 'kcal') {
+            barsHtml.push(<Bar dataKey="kcal" fill="#18139c" stroke="#18139c" stackId="a" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="goalKcal" fill="none" stroke="#18139c" stackId="a" key={this.getKey()}
+                               isAnimationActive={false}/>);
         } else {
-            barsHtml.push(<Bar dataKey="carbs" stackId="a" fill="#d67e0f" stroke="#d67e0f" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="goalCarbs" stackId="a" fill="none" stroke="#d67e0f" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="protein" stackId="b" fill="#ab1d1d" stroke="#ab1d1d" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="goalProtein" stackId="b" fill="none" stroke="#ab1d1d" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="fibre" stackId="c" fill="#1c7800" stroke="#1c7800" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="goalFibre" stackId="c" fill="none" stroke="#1c7800" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="fat" stackId="d" fill="saddlebrown" stroke="saddlebrown" key={this.getKey()} isAnimationActive={false}/>);
-            barsHtml.push(<Bar dataKey="goalFat" stackId="d" fill="none" stroke="saddlebrown" key={this.getKey()} isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="carbs" stackId="a" fill="#d67e0f" stroke="#d67e0f" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="goalCarbs" stackId="a" fill="none" stroke="#d67e0f" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="protein" stackId="b" fill="#ab1d1d" stroke="#ab1d1d" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="goalProtein" stackId="b" fill="none" stroke="#ab1d1d" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="fibre" stackId="c" fill="#1c7800" stroke="#1c7800" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="goalFibre" stackId="c" fill="none" stroke="#1c7800" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="fat" stackId="d" fill="saddlebrown" stroke="saddlebrown" key={this.getKey()}
+                               isAnimationActive={false}/>);
+            barsHtml.push(<Bar dataKey="goalFat" stackId="d" fill="none" stroke="saddlebrown" key={this.getKey()}
+                               isAnimationActive={false}/>);
         }
 
         return (
@@ -151,7 +127,10 @@ class ViewDiaryEntries extends Component {
                 <Calendar onChange={this.handleChange} selectRange={this.state.multipleSelection}
                           value={this.state.date}/>
                 <button onClick={this.handleSubmit}>Submit</button>
-                <button onClick={() => {console.log(this.props.searchedDiaryEntries)}}>Debug</button>
+                <button onClick={() => {
+                    console.log(this.state)
+                }}>Debug
+                </button>
 
                 <select value={this.state.show} onChange={this.handleSelect}>
                     <option value="kcal">Energeticka hodnota</option>
@@ -161,24 +140,79 @@ class ViewDiaryEntries extends Component {
                 <div style={{display: displayChart}}>
                     <BarChart width={730} height={250} data={this.state.data}>
                         <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="date"/>
-                        <YAxis/>
-                        <Tooltip content={<CustomTooltip />}/>
+                        <XAxis dataKey="date" label={{ value: 'Data', position: 'insideBottomRight', offset: -5 }} />
+                        <YAxis label={{ value: this.state.show==="kcal" ? "kcal":"g",
+                            position: 'insideLeft', offset: this.state.show==="kcal" ? -15:5}} />
+                        <Tooltip content={<CustomTooltip/>}/>
                         <Legend formatter={this.renderLegendText}/>
                         {barsHtml}
-                    </BarChart>;
+                    </BarChart>
                 </div>
 
+                {this.state.tableHtml}
+
             </div>
-        )
+        );
+    }
+
+    generateTable = (data) => {
+        let cells;
+        let rows = [];
+        let mealNames = ["Snidane", "Dopoledni svacina", "Obed", "Odpoledni svacina", "Vecere", "Ostatni"];
+        let nuritionNames = ["Chod", "Energeticka hodnota", "Bilkoviny", "Sacharidy", "Tuky", "Vlaknina", "Obsah chodu"];
+        let meals = data.meals;
+        let activities = data.activities;
+
+        for (let i = 0; i < mealNames.length + 1; i++) {
+            let mealEntry;
+            let mealContent = "";
+            if (i > 0) {
+                mealEntry = Object.entries(meals[i - 1]);
+            }
+            cells = [];
+            for (let j = 0; j < nuritionNames.length; j++) {
+                if (i === 0) {
+                    cells.push(<th key={this.getKey()}>{nuritionNames[j]}</th>);
+                } else if (j === 0) {
+                    cells.push(<td key={this.getKey()}>{mealNames[i - 1]}</td>);
+                } else {
+                    if (j === 1) {
+                        cells.push(<td key={this.getKey()}>{mealEntry[j - 1][1] + " kcal"}</td>);
+                    } else if (j > 1 && j < mealNames.length) {
+                        cells.push(<td key={this.getKey()}>{mealEntry[j - 1][1] + " g"}</td>);
+                    } else {
+                        // eslint-disable-next-line
+                        mealEntry[j - 1][1].forEach(meal => {
+                            mealContent += meal + ", ";
+                        });
+                        mealContent = mealContent.substring(0, mealContent.length - 2);
+                        cells.push(<td key={this.getKey()}>{mealContent}</td>);
+                    }
+                }
+            }
+            rows.push(<tr key={this.getKey()}>{cells}</tr>);
+        }
+
+        let table = <div>
+            <table>
+                <tbody>{rows}</tbody>
+            </table>
+            <div>
+                {"Kalorií spáleno aktivitami: " + activities.kcal}
+                <p/>
+                {"Popis aktivit: " + activities.description}
+            </div>
+        </div>;
+
+        this.setState({tableHtml: table})
     }
 }
 
 const handleInflection = (number) => {
     let inflection;
-    if(parseInt(number) === 1){
+    if (parseInt(number) === 1) {
         inflection = "gram";
-    } else if(parseInt(number) > 1 && parseInt(number) < 5){
+    } else if (parseInt(number) > 1 && parseInt(number) < 5) {
         inflection = "gramy"
     } else {
         inflection = "gramů"
@@ -187,24 +221,37 @@ const handleInflection = (number) => {
     return inflection;
 };
 
-const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload.length===2) {
+const CustomTooltip = ({active, payload, label}) => {
+    if (active && payload.length === 2) {
+        let responseString = "";
+        if(payload[1].value<0) {
+            responseString = `Energie: Zkonzumováno ${payload[0].value} kcal, více než cíl o ${-payload[1].value} kcal`;
+        }else {
+            responseString = `Energie: Zkonzumováno ${payload[0].value} kcal, do cíle zbývá ${payload[1].value} kcal`;
+        }
         return (
-            <div className="custom-tooltip" style={{"background-color":"white"}}>
+            <div className="custom-tooltip" style={{"background-color": "white"}}>
                 <p className="label">{`${label}:`}</p>
-                <p>{`Energie: Zkonzumováno ${payload[0].value} kcal, do cíle zbývá ${payload[1].value} kcal`}</p>
+                <p>{responseString}</p>
             </div>
         );
-    } else if(active && payload.length===8){
+    } else if (active && payload.length === 8) {
         let names = ["Sacharidy", "Bílkoviny", "Vláknina", "Tuky"];
         let html = [];
-        for(let i = 0; i<8; i+=2){
-            html.push(<p key={i}>{`${names[i/2]}: Zkonzumováno ${payload[i].value} ${handleInflection(payload[i].value)},
-              do cíle zbývá ${payload[i+1].value} ${handleInflection(payload[i+1].value)}`}</p>);
+        for (let i = 0; i < 8; i += 2) {
+            if (payload[i + 1].value < 0) {
+                html.push(<p
+                    key={i}>{`${names[i / 2]}: Zkonzumováno ${payload[i].value} ${handleInflection(payload[i].value)},
+              více než cíl o ${-payload[i + 1].value} ${handleInflection(payload[i + 1].value)}`}</p>);
+            } else {
+                html.push(<p
+                    key={i}>{`${names[i / 2]}: Zkonzumováno ${payload[i].value} ${handleInflection(payload[i].value)},
+              do cíle zbývá ${payload[i + 1].value} ${handleInflection(payload[i + 1].value)}`}</p>);
+            }
         }
 
         return (
-            <div className="custom-tooltip" style={{"background-color":"white"}}>
+            <div className="custom-tooltip" style={{"background-color": "white"}}>
                 <p className="label">{`${label}:`}</p>
                 {html}
             </div>
